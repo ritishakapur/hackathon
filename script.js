cbeccbf14d416c9420ca92a94818f98a3807a9fc
Ritisha <ritishakapur@users.noreply.github.com>
2026-01-17 00:51:33 +0530

    landing page with css

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".primary").onclick = () => {
+    alert("start study session (chunking + 5 min flow)");
+};
+
+document.querySelector(".secondary").onclick = () => {
+    alert("study capsule focus mode");
+};
